c5dd8ffefdbf07cb68a919572e5ef950e380b4c7
unknown <swastik.sonkusare18@gmail.com>
2021-02-13 21:19:39 +0530

    implemented admin functionality for userEdit, userList, productEdit, productList, image upload using multer

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { authUser, getUserProfile, registerUser, updateUserProfile, getUsers } from '../controllers/userController.js'
+import { authUser, getUserProfile, registerUser, updateUserProfile, getUsers, deleteUser, getUserById, updateUser } from '../controllers/userController.js'
 import { protect, isAdmin } from '../middleware/authMiddleware.js'
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.route('/').post(registerUser).get(protect, isAdmin, getUsers)
 router.post('/login', authUser);
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
+router.route('/:id').delete(protect, isAdmin, deleteUser).get(protect, isAdmin, getUserById).put(protect, isAdmin, updateUser)
 
 export default router;
--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -5,21 +5,33 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import Message from '../components/Message';
 import Loader from '../components/Loader';
-import { listUsers } from '../actions/userAction';
+import { listUsers, deleteUser } from '../actions/userAction';
 
-const UserListScreen = () => {
+const UserListScreen = ({ history }) => {
     const dispatch = useDispatch()
 
     const userList = useSelector(state => state.userList);
     const { error,loading, users } = userList;
 
+    const userLogin = useSelector(state => state.userLogin);
+    const { userInfo } = userLogin;
+
+    const userDelete = useSelector(state => state.userDelete);
+    const { success: successDelete } = userDelete;
+
     useEffect(() =>{
-        dispatch(listUsers())
-    }, [dispatch])
+        if(userInfo && userInfo.isAdmin) {
+            dispatch(listUsers())
+        } else {
+            history.push('/login')
+        }
+    }, [dispatch, userInfo, history, successDelete])
 
 
     const deleteHandler = (id)=>{
-        console.log('delete')
+        if(window.confirm('Are you sure?')) {
+            dispatch(deleteUser(id))
+        }
     }
 
     return (
@@ -49,7 +61,7 @@ const UserListScreen = () => {
                                     )}
                                 </td>
                                 <td>
-                                    <LinkContainer to={`/user/${user._id}/edit`}>
+                                    <LinkContainer to={`/admin/user/${user._id}/edit`}>
                                         <Button variant='light' className='btn-sm'>
                                             <i className='fas fa-edit'></i>
                                         </Button>
